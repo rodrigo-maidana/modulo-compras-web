7fde35b90bd67d51f63b225a65b3ca0942015c1e
Rodrigo Maidana <114775631+romaidana29@users.noreply.github.com>
2024-05-03 01:43:02 -0400

    chore: Update ModalPedidosDetalles component formatting and imports

--- src/components/ModalPedidosDetalles.jsx
+++ src/components/ModalPedidosDetalles.jsx
@@ -1,16 +1,12 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal";
-import App from "../App";
 
-
-import React from 'react'
+import React from "react";
 
 const ModalPedidosDetalles = (id) => {
   return (
     <div>
-            <h2>Categorías</h2>
-      <table className='table table-dark table-striped table-bordered'>
+      <h2>Categorías</h2>
+      <table className="table table-dark table-striped table-bordered">
         <thead>
           <tr>
             <th>ID</th>
@@ -19,21 +15,30 @@ const ModalPedidosDetalles = (id) => {
           </tr>
         </thead>
         <tbody>
-          {categorias.map(categoria => (
+          {categorias.map((categoria) => (
             <tr key={categoria.id}>
               <td>{categoria.id}</td>
               <td>{categoria.nombre}</td>
               <td>
-                <button onClick={() => eliminarCategoria(categoria.id)} className='btn btn-danger'>Eliminar</button>
-                <button onClick={() => modificarCategoria(categoria)} className='btn btn-primary'>Modificar</button>
+                <button
+                  onClick={() => eliminarCategoria(categoria.id)}
+                  className="btn btn-danger"
+                >
+                  Eliminar
+                </button>
+                <button
+                  onClick={() => modificarCategoria(categoria)}
+                  className="btn btn-primary"
+                >
+                  Modificar
+                </button>
               </td>
             </tr>
           ))}
         </tbody>
       </table>
-      
     </div>
-  )
-}
+  );
+};
 
-export default ModalPedidosDetalles
+export default ModalPedidosDetalles;
